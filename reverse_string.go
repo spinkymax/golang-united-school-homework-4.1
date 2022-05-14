ab00fadc11e0a2b2b839bb3340d0fe7115c0be6c
spinkymax <sp1nk@mail.ru>
2022-05-14 23:25:58 +0300

    Reverse

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,13 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	var s = []byte(input)
+	for i := 0; i < len(s)-1; i++ {
+		for j := 0; j < len(s)-i-1; j++ {
+			s[j], s[j+1] = s[j+1], s[j]
+		}
+}
+	return (string(s))
+
+
 }
