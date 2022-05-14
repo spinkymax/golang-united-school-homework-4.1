eebdd4453ff0764ab2ac89240e923e505936368a
spinkymax <99490816+spinkymax@users.noreply.github.com>
2022-05-14 23:51:31 +0300

    Update reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -1,15 +1,11 @@
 package reverse_string
 
-import "github.com/kyokomi/emoji/v2"
-
 func ReverseString(input string) (output string) {
-	var s = []byte(input)
+	var s = []rune(input)
 	for i := 0; i < len(s)-1; i++ {
 		for j := 0; j < len(s)-i-1; j++ {
 			s[j], s[j+1] = s[j+1], s[j]
 		}
 }
 	return (string(s))
-
-
 }
